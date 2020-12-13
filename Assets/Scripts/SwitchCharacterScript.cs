bf0444c7d821a698c7ae7eb93aa1d0f714f97c54
Sirius Victoria <pelinalvo@gmail.com>
2020-12-13 14:26:16 +0300

    Karakter değişimi düzeltildi

--- Assets/Scripts/SwitchCharacterScript.cs
+++ Assets/Scripts/SwitchCharacterScript.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class SwitchCharacterScript : MonoBehaviour
 {
     public GameObject avatar1, avatar2, avatar3;
+    public int currentCharacterIndex = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-
         avatar1.gameObject.SetActive(true);
         avatar2.gameObject.SetActive(false);
         avatar3.gameObject.SetActive(false);
@@ -20,36 +20,55 @@ public class SwitchCharacterScript : MonoBehaviour
     {
         if (Input.GetKey("1"))
         {
-            avatar1.transform.position = avatar2.transform.position;
-            avatar1.transform.position = avatar3.transform.position;
-            avatar1.transform.rotation = avatar2.transform.rotation;
-            avatar1.transform.rotation = avatar3.transform.rotation;
+            if (currentCharacterIndex == 2)
+            {
+                 avatar1.transform.position = avatar2.transform.position;
+                 avatar1.transform.rotation = avatar2.transform.rotation;
+            }
+            else if (currentCharacterIndex == 3)
+            {
+                avatar1.transform.position = avatar3.transform.position;
+                avatar1.transform.rotation = avatar3.transform.rotation;
+            }
+
             avatar1.gameObject.SetActive(true);
             avatar2.gameObject.SetActive(false);
             avatar3.gameObject.SetActive(false);
-
+            currentCharacterIndex = 1;
 
         }
 
         if (Input.GetKey("2"))
         {
-            avatar2.transform.position = avatar1.transform.position;
-            avatar2.transform.position = avatar3.transform.position;
-            avatar2.transform.rotation = avatar1.transform.rotation;
-            avatar2.transform.rotation = avatar3.transform.rotation;
+            if (currentCharacterIndex == 1)
+            {
+                avatar2.transform.position = avatar1.transform.position;
+                avatar2.transform.rotation = avatar1.transform.rotation;
+            }
+            else if (currentCharacterIndex == 3)
+            {
+                avatar2.transform.position = avatar3.transform.position;
+                avatar2.transform.rotation = avatar3.transform.rotation;
+            }
+
             avatar1.gameObject.SetActive(false);
             avatar2.gameObject.SetActive(true);
             avatar3.gameObject.SetActive(false);
-
 
         }
 
         if (Input.GetKey("3"))
         {
-            avatar3.transform.position = avatar1.transform.position;
-            avatar3.transform.position = avatar2.transform.position;
-            avatar3.transform.rotation = avatar1.transform.rotation;
-            avatar3.transform.rotation = avatar2.transform.rotation;
+            if (currentCharacterIndex == 1)
+            {
+                avatar3.transform.position = avatar1.transform.position;
+                avatar3.transform.rotation = avatar1.transform.rotation;
+            }
+            else if (currentCharacterIndex == 2)
+            {
+                avatar3.transform.position = avatar2.transform.position;
+                avatar3.transform.rotation = avatar2.transform.rotation;
+            }
 
             avatar1.gameObject.SetActive(false);
             avatar2.gameObject.SetActive(false);
